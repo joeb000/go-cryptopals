0c90a0131ccf46e6f38a4bb4d7a85ea2d164cd0b
joeb000 <j.b5286@gmail.com>
2016-09-29 00:37:34 -0400

    started 6

---
+++ problem5/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+)
+
+func main() {
+	line := "Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal"
+	expected := "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+	byteLine := []byte(line)
+	keyStr := []byte("ICE")
+	fmt.Printf("Result: %x\n", repKeyXOR(byteLine, keyStr))
+	fmt.Printf("Expect: %v\n", expected)
+}
+
+func repKeyXOR(b, key []byte) []byte {
+	crypt := make([]byte, len(b))
+	for i := 0; i < len(b); i++ {
+		crypt[i] = xor(b[i], key[i%3])
+	}
+	return crypt
+}
+
+func xor(b, key byte) byte {
+	return b ^ key
+}
