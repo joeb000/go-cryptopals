33d8504380311f13116abd0b6c9b2bff544e6a7c
Bernitt <j.b5286@gmail.com>
2016-09-29 11:58:48 -0400

    reorganizing dirs and solving first half of p6

---
+++ 6-breakRepeatingKeyXOR/pre6.go
@@ -0,0 +1,50 @@
+package breakRepeatingKeyXOR
+
+import (
+	"encoding/hex"
+	"fmt"
+)
+
+func prereq() bool {
+	test := "this is a test"
+	wokka := "wokka wokka!!!"
+	expected := 37
+	aa, _ := hex.DecodeString(hex.EncodeToString([]byte(test)))
+	bb, _ := hex.DecodeString(hex.EncodeToString([]byte(wokka)))
+
+	fmt.Printf("test:     %x\n", aa)
+	fmt.Printf("woka:     %x\n", bb)
+	fmt.Printf("Hamming Distance: %v\n", calculateHammingDistance(aa, bb))
+	if calculateHammingDistance(aa, bb) == expected {
+		return true
+	} else {
+		return false
+	}
+}
+
+func calculateHammingDistance(a []byte, b []byte) int {
+	prod := xor(a, b)
+
+	return countSetBits(prod)
+}
+
+// Brian Kernighan's method of counting set bits
+// See https://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetKernighan
+func countSetBits(byteArray []byte) int {
+	setBits := 0
+	for _, b := range byteArray {
+		for b != 0 {
+			b &= b - 1 //bitwise & operator against byte-1
+			setBits++
+		}
+	}
+	return setBits
+}
+
+func xor(hex []byte, key []byte) []byte {
+	res := make([]byte, len(hex))
+	for i := 0; i < len(hex); i++ {
+		res[i] = hex[i] ^ key[i]
+	}
+	return res
+}
